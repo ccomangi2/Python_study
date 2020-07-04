4bada63296aad95038d064f2dae0fe14a9ae8db9
ccomangi2 <ccomangi2@gmail.com>
2020-07-04 12:04:34 +0900

    unit 4.1

--- venv/4.1 세미콜론.py
+++ venv/4.1 세미콜론.py
@@ -1,13 +1,2 @@
-def func_a(k):
-    sum = 0
-    for i in range(0,k+1):
-        sum += i
-    return sum
-
-def solution(n, m):
-    sum_to_m = func_a(m)
-    sum_to_n = func_a(n-1)
-    answer = sum_to_m - sum_to_n
-    return answer
-
-print(solution(5,10))
+print('Hello'); print('1234')
+# 여러 줄 코딩 가능
