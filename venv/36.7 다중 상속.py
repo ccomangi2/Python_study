bb13c694b0c5a3c819f8cca588fbce6e0ae15c12
ccomangi2 <ccomangi2@gmail.com>
2020-07-14 12:27:01 +0900

    unit 36.7

--- venv/36.7 다중 상속.py
+++ venv/36.7 다중 상속.py
@@ -15,3 +15,23 @@ james = Undergraduate()
 james.greeting()  # 안녕하세요.: 기반 클래스 Person의 메서드 호출
 james.manage_credit()  # 학점 관리: 기반 클래스 University의 메서드 호출
 james.study()  # 공부하기: 파생 클래스 Undergraduate에 추가한 study 메서드
+
+# 다이아몬드 상속
+
+class A:
+    def greeting(self):
+        print('안녕하세요. A입니다.')
+
+class B(A):
+    def greeting(self):
+        print('안녕하세요. B입니다.')
+
+class C(A):
+    def greeting(self):
+        print('안녕하세요. C입니다.')
+
+class D(B, C):
+    pass
+
+x = D()
+x.greeting()  # 안녕하세요. B입니다.
