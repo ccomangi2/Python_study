1794d2a0513f4d9125f149d1f0163eec513b78d3
ccomangi2 <ccomangi2@gmail.com>
2020-07-25 16:49:57 +0900

    unit 19.4

--- venv/19.4 역삼각형 별 출력.py
+++ venv/19.4 역삼각형 별 출력.py
@@ -5,3 +5,11 @@ for i in range(5):
         else:
             print('*', end='')
     print()
+
+    for i in range(5):
+        for j in range(5):
+            if j < i:
+                print(' ', end='')
+            else:
+                print('*', end='')
+        print()
