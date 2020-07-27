e2d62a28cef2a90c6bef102f4ea4320bbdea1df4
ccomangi2 <ccomangi2@gmail.com>
2020-07-27 19:48:37 +0900

    기말고사 대비 3.1

---
+++ venv/기말고사/3.1 리스트.py
@@ -0,0 +1,8 @@
+# 리스트 생성
+l = []                                  #빈 리스트
+player = ["Messi", 10, True]            #["Messi", 10, True]
+list("Happy")                           #['H', 'a', 'p', 'p', 'y']
+list((1125, 2436))                      #[1125, 2436]
+list({"menu":"pizze", "price":10000})   #['menu', 'price']
+list({"사과", "배"})                     #['사과', '배']
+nums = list(range(3))                   #[0, 1, 2]
