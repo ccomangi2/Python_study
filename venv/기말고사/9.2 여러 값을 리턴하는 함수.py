c0d4140b0a19a785718d5be4bd9128deac7ebefa
ccomangi2 <ccomangi2@gmail.com>
2020-07-28 22:34:04 +0900

    기말고사 대비 9.2

---
+++ venv/기말고사/9.2 여러 값을 리턴하는 함수.py
@@ -0,0 +1,29 @@
+# 리스트를 사용해 여러 값을 하나로 묶어 리턴하기
+def multi_num(multi, start, end):
+    result = []
+    for n in range(start, end):
+        if n % multi == 0:
+            result.append(n)
+    return result
+
+multi2 = multi_num(17,1,200)
+print("multi_num(17,1,200) : ", multi2)
+print()
+multi3 = multi_num(3,1,100)
+print("multi_num(3,1,100) : ", multi3)
+
+
+# 튜플을 사용해 여러 값을 여러 개 리턴하기
+def min_max(*args):
+    min = args[0]
+    max = args[0]
+    for arg in args:
+        if min > arg:
+            min = arg
+        if max < arg:
+            max = arg
+    return min, max
+print(min_max(52, -3, 23, 89, -21))
+min_value, max_value = min_max(52, -3, 23, 89, -21)
+print("최젓값:", min_value)
+print("최곳값:", max_value)
